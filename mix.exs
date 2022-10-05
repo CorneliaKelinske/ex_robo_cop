8b10b65cd79d287e19b24b8c667dc51d598d2327
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-10-05 14:06:47 -0700

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExRoboCop.MixProject do
   def project do
     [
       app: :ex_robo_cop,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
