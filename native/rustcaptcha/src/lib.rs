bff988461ca786401d447be737f0046c0de60262
cornelia <corneliakelinske@gmail.com>
2024-03-25 15:02:21 -0700

    update rust code

--- native/rustcaptcha/src/lib.rs
+++ native/rustcaptcha/src/lib.rs
@@ -1,8 +1,14 @@
-#[rustler::nif]
+use base64::{Engine as _, engine::general_purpose};
 
+#[rustler::nif]
 fn generate() -> Option<(String, String)> {
     match captcha::gen(captcha::Difficulty::Easy).as_tuple() {
-        Some((string, data)) => Some((string, base64::encode(&data))),
+        
+        Some((string, data)) => {
+            let data = general_purpose::STANDARD.encode(data);
+            Some((string, data))
+        }
+        
         None => None,
     }
 }
